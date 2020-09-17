7eafd732e8c25da0c3fd167aa545f4051e5374da
Ket Patel <ket.patel610@gmail.com>
2020-09-17 10:22:08 +0530

    Updates

---
+++ precache-manifest.2ae8ce6d0a4beb9e0c7a8698c6dc0d56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f2cf57139f78e524d3bab5a9c9a1f7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "85b084fb45425f0f5aea",
+    "url": "/monsters-rolodex/static/css/main.d4e4ae15.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b084fb45425f0f5aea",
+    "url": "/monsters-rolodex/static/js/main.7b94029c.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
